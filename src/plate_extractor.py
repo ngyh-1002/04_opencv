0c470cde2ba54a33d8509f0ade9fe6d36929988a
ngyh-1002 <01049200297aa@gmail.com>
2025-07-31 11:49:56 +0900

    마우스로 기준점을 초기화할 함수구현
    마우스로 기준점을 초기화할 함수구현

--- src/plate_extractor.py
+++ src/plate_extractor.py
@@ -7,3 +7,13 @@ rows, cols = img.shape[:2]
 draw = img.copy()
 pts_cnt = 0
 pts = np.zeros((4,2), dtype=np.float32)
+
+def onMouse(event, x, y, flags, param):  #마우스 이벤트 콜백 함수 구현 ---① 
+    global  pts_cnt                     # 마우스로 찍은 좌표의 갯수 저장
+    if event == cv2.EVENT_LBUTTONDOWN:  
+        cv2.circle(draw, (x,y), 10, (0,255,0), -1) # 좌표에 초록색 동그라미 표시
+        cv2.imshow(win_name, draw)
+
+        pts[pts_cnt] = [x,y]            # 마우스 좌표 저장
+        pts_cnt+=1
+        
